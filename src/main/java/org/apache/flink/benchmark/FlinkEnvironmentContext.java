51f04c896b34273368901b56175dfe024b862973
Piotr Nowojski <piotr.nowojski@gmail.com>
2017-11-28 10:48:47 +0100

    Extract BenchmarkBase for non Flink integration benchmarks

---
+++ src/main/java/org/apache/flink/benchmark/FlinkEnvironmentContext.java
@@ -0,0 +1,35 @@
+package org.apache.flink.benchmark;
+
+import org.apache.flink.runtime.state.memory.MemoryStateBackend;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.io.IOException;
+
+import static org.openjdk.jmh.annotations.Scope.Thread;
+
+@State(Thread)
+public class FlinkEnvironmentContext {
+    public final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+    private final int parallelism = 1;
+    private final boolean objectReuse = true;
+
+    @Setup
+    public void setUp() throws IOException {
+        // set up the execution environment
+        env.setParallelism(parallelism);
+        env.getConfig().disableSysoutLogging();
+        if (objectReuse) {
+            env.getConfig().enableObjectReuse();
+        }
+
+        env.setStateBackend(new MemoryStateBackend());
+    }
+
+    public void execute() throws Exception {
+        env.execute();
+    }
+}
