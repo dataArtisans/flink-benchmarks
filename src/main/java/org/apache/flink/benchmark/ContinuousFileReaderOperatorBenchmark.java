530b300913a06d4b9519733ece9c658aa244d64d
Roman Khachatryan <khachatryan.roman@gmail.com>
2019-11-19 11:25:44 +0100

    [FLINK-14783] CFRO benchmark: mock data source (instead of files)

--- src/main/java/org/apache/flink/benchmark/ContinuousFileReaderOperatorBenchmark.java
+++ src/main/java/org/apache/flink/benchmark/ContinuousFileReaderOperatorBenchmark.java
@@ -17,55 +17,33 @@
 
 package org.apache.flink.benchmark;
 
+import joptsimple.internal.Strings;
 import org.apache.flink.api.common.io.FileInputFormat;
-import org.apache.flink.api.java.io.TextInputFormat;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.core.fs.FileInputSplit;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
-import org.apache.flink.util.Preconditions;
+import org.apache.flink.streaming.api.functions.source.ContinuousFileReaderOperator;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
+import org.apache.flink.streaming.api.functions.source.TimestampedFileInputSplit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.OperationsPerInvocation;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 import org.openjdk.jmh.runner.options.VerboseMode;
 
-import java.io.File;
-import java.nio.file.Paths;
-
-/**
- * Measures simple file reading and, optionally, parsing operation.
- * Defaults to reading 100000 records from: <ol>
- *     <li>100 text files (1000 lines each)</li>
- *     <li>1000 text (smaller) files (100 lines each)</li></ol>
- * <p>
- * Parameters: <ul>
- *     <li>path - folder or file; if not absolute, it's assumed relative to src/main/resources </li>
- *     <li>parallelism - degree of parallelism.</li>
- * </ul>
- */
-// bash script to generate 100 files with 1000 random base64 lines each:
-// for f in $(seq 1 100); do for l in $(seq 1 1000); do dd if=/dev/random count=1 | base64 >> src/main/resources/txt-100-1000-10/$f; done; done
-// for f in $(seq 1 1000); do for l in $(seq 1 100); do dd if=/dev/random count=1 | base64 >> src/main/resources/txt-1000-100-10/$f; done; done
 @OperationsPerInvocation(value = ContinuousFileReaderOperatorBenchmark.RECORDS_PER_INVOCATION)
 public class ContinuousFileReaderOperatorBenchmark extends BenchmarkBase {
-    public static final int RECORDS_PER_INVOCATION = 1000;
+    private static final int SPLITS_PER_INVOCATION = 100;
+    private static final int LINES_PER_SPLIT = 20_000_000;
+    public static final int RECORDS_PER_INVOCATION = SPLITS_PER_INVOCATION * LINES_PER_SPLIT;
 
-    private static final long CHECKPOINT_INTERVAL_MS = 100;
-
-    private final String BASE_DIR = "src/main/resources/"; // used if non-absolute path provided
-
-    private File absPath;
-    private FileInputFormat<?> fileReader;
-
-    @Param({"txt-100-1000-10", "txt-1000-100-10"})
-    public String path;
-
-    @Param("1")
-    public String parallelism;
+    private static final TimestampedFileInputSplit SPLIT = new TimestampedFileInputSplit(0, 0, new Path("."), 0, 0, new String[]{});
+    private static final String LINE = Strings.repeat('0', 10);
 
     public static void main(String[] args)
             throws RunnerException {
@@ -77,30 +55,64 @@ public class ContinuousFileReaderOperatorBenchmark extends BenchmarkBase {
         new Runner(options).run();
     }
 
-    @Setup
-    public void setUp() {
-        absPath = buildPath();
-        Preconditions.checkArgument(absPath.exists(), "%s doesn't exist", absPath);
-        if (absPath.isDirectory()) Preconditions.checkArgument(absPath.canExecute(), "can't list files in %s", absPath);
-        else Preconditions.checkArgument(absPath.canRead(), "can't read %s", absPath);
-        fileReader = new TextInputFormat(new Path(absPath.toString()));
-    }
-
     @Benchmark
     public void readFiles(FlinkEnvironmentContext context) throws Exception {
         StreamExecutionEnvironment env = context.env;
-
-        env.enableCheckpointing(CHECKPOINT_INTERVAL_MS)
-                .setParallelism(Integer.parseInt(parallelism))
-                .readFile(fileReader, absPath.toString())
+        env
+                .enableCheckpointing(100)
+                .setParallelism(1)
+                .addSource(buildSourceFun())
+                .transform("fileReader", TypeInformation.of(String.class),
+                        new ContinuousFileReaderOperator<>(new MockInputFormat()))
                 .addSink(new DiscardingSink<>());
 
         env.execute();
     }
 
-    private File buildPath() {
-        java.nio.file.Path p = this.path.startsWith("/") ? Paths.get(this.path) : Paths.get(BASE_DIR, this.path);
-        return p.toAbsolutePath().toFile();
+    private SourceFunction<TimestampedFileInputSplit> buildSourceFun() {
+        return new SourceFunction<TimestampedFileInputSplit>() {
+            private volatile boolean isRunning = true;
+            private int count = 0;
+
+            @Override
+            public void run(SourceContext<TimestampedFileInputSplit> ctx) {
+                while (isRunning && count < SPLITS_PER_INVOCATION) {
+                    count++;
+                    synchronized (ctx.getCheckpointLock()) {
+                        ctx.collect(SPLIT);
+                    }
+                }
+            }
+
+            @Override
+            public void cancel() {
+                isRunning = false;
+            }
+        };
     }
 
+    private static class MockInputFormat extends FileInputFormat<String> {
+        private transient int count = 0;
+
+        @Override
+        public boolean reachedEnd() {
+            return count >= ContinuousFileReaderOperatorBenchmark.LINES_PER_SPLIT;
+        }
+
+        @Override
+        public String nextRecord(String s) {
+            count++;
+            return LINE;
+        }
+
+        @Override
+        public void open(FileInputSplit fileSplit) {
+            // prevent super from accessing file
+        }
+
+        @Override
+        public void configure(Configuration parameters) {
+            // prevent super from requiring certain settings (input.file.path)
+        }
+    }
 }
